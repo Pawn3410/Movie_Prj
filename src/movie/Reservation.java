a1b8a4af7e14f0c86849513c2e419ce30fac6d7d
PAWN <pawn3410@gmail.com>
2025-11-17 15:41:56 +0900

    윤희남 5 예매 코드 추가

--- src/movie/Reservation.java
+++ src/movie/Reservation.java
@@ -5,7 +5,7 @@ public class Reservation {
     public boolean reserve_5(Movie movie, int count) {
         if (movie.reserveSeat(count)) {
             System.out.println("✅ 예매 성공: " + movie.getTitle() + ", 수량: " + count);
-            System.out.println("서연경");
+            System.out.println("윤희남");
             return true;
         } else {
             System.out.println("❌ 예매 실패: 좌석 부족");
@@ -14,6 +14,6 @@ public class Reservation {
     }
 
     public void reserve1842() {
-        System.out.println("예매 완료!_이민정");
+        System.out.println("예매 완료!_윤희남");
     }
 }
